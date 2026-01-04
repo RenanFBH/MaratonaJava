9b5c045888b6a72f5f091cd1f7098f2ab8cf3a16
RenanFBH <renan.f.bezerra10@gmail.com>
2026-01-04 10:51:51 -0300

    Exceptions parte 2

---
+++ src/teste/maratonajava/javacore/Oexception/exception/test/ExceptionTest02.java
@@ -0,0 +1,24 @@
+package teste.maratonajava.javacore.Oexception.exception.test;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExceptionTest02 {
+    public static void main(String[] args) throws IOException {
+        criarNovoArquivo();
+    }
+
+    public static void criarNovoArquivo() throws IOException {
+        File file = new File("arquivo\\teste.txt");
+        try {
+            boolean isCriado = file.createNewFile();
+            System.out.println("Arquivo criado " + isCriado);
+            boolean isDeletado = file.delete();
+            System.out.println("Arquivo deletado " + isDeletado);
+        } catch(IOException e) {
+            e.printStackTrace();
+            throw e;
+        }
+
+    }
+}
