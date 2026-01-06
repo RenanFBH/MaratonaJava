07ef7e9affc1721fed44390f24b14dd0c7e7150e
RenanFBH <renan.f.bezerra10@gmail.com>
2026-01-06 18:03:16 -0300

    Exceptions parte 3

---
+++ src/teste/maratonajava/javacore/Oexception/exception/dominio/Funcionario.java
@@ -0,0 +1,11 @@
+package teste.maratonajava.javacore.Oexception.exception.dominio;
+
+import java.io.FileNotFoundException;
+
+public class Funcionario extends Pessoa {
+
+    public void salvar() throws LoginInvalidoException, FileNotFoundException {
+        System.out.println("Salvando funcionário");
+    }
+
+}
---
+++ src/teste/maratonajava/javacore/Oexception/exception/dominio/Pessoa.java
@@ -0,0 +1,11 @@
+package teste.maratonajava.javacore.Oexception.exception.dominio;
+
+import java.io.FileNotFoundException;
+
+public class Pessoa {
+
+    public void salvar() throws LoginInvalidoException, FileNotFoundException {
+        System.out.println("Salvando pessoa");
+    }
+
+}
